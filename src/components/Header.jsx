b64bcdd9e16385cb104e412fe8d158102d397e7b
githendragowdakr@gmail.com <githendragowdakr@gmail.com>
2025-09-15 11:25:53 +0530

    Created Header Component

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom";
+import profileIcon from "../assets/profileIcon.svg";
+
+const Header = () => {
+  return (
+    <div className="flex flex-row w-screen h-16 shadow-[0_4px_6px_-1px_rgba(0,0,0,0.1)] p-4">
+      <div className="flex items-center">
+        <h1 className="font-medium text-xl ml-10 mr-96">Dashboard</h1>
+        <Link to="/">
+          <img src={profileIcon} alt="Profile Icon" className="ml-96 pr-20" />
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
